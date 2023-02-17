e3f396bc49d53e05fd57d0f5643d34d5bea4693f
thiagofm2 <106693084+thiagofm2@users.noreply.github.com>
2023-02-16 23:01:25 -0300

    criacao pag home

---
+++ nuxt-app/app.vue
@@ -0,0 +1,9 @@
+<template>
+  <NuxtPage />
+</template>
+
+<script setup>
+</script>
+
+<style>
+</style>
---
+++ nuxt-app/pages/home.vue
@@ -0,0 +1,11 @@
+<script setup>
+</script>
+
+<style scoped>
+</style>
+
+<template>
+    <v-container fluid>
+        <v-btn>TESTE</v-btn>
+    </v-container>
+</template>
